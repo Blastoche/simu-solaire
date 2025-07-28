49ce150651bedc3684e939032f69f6859fc460d0
Blastoche <bastien.laffargue33@gmail.com>
2025-07-28 14:59:49 +0200

    Create tariffs.py

---
+++ config/tariffs.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Tarifs et aides financières (2025)
+Source : photovoltaique.info
+"""
+
+TARIFFS = {
+    "electricity": {
+        "purchase": 0.20,  # €/kWh
+        "sell": {
+            "≤3kWc": 0.04,
+            "3-9kWc": 0.04,
+            "9-36kWc": 0.0731
+        }
+    },
+    "subsidies": {
+        "autoconsommation": {  # €/kWc
+            "≤3kWc": 80,
+            "3-9kWc": 80
+        },
+        "tva_reduced": 0.10  # TVA à 10%
+    }
+}
