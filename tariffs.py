fc9706ebc48c088c5e7d65e5380a7e017323b8b7
Blastoche <bastien.laffargue33@gmail.com>
2025-07-28 11:06:11 +0200

    Update tariffs.py

--- tariffs.py
+++ tariffs.py
@@ -1,21 +1,27 @@
-# Tarifs officiels 2025
+# -*- coding: utf-8 -*-
+"""
+Tarifs et aides financières (2025)
+Source : photovoltaique.info
+"""
+
 TARIFFS = {
     "electricity": {
-        "purchase": 0.20,          # €/kWh acheté
-        "surplus_sell": {          # Tarifs de rachat
+        "purchase": 0.20,  # €/kWh
+        "sell": {
             "≤3kWc": 0.04,
             "3-9kWc": 0.04,
             "9-36kWc": 0.0731
         }
     },
     "subsidies": {
-        "autoconsommation": {      # Prime à l'installation
+        "autoconsommation": {  # €/kWc
             "≤3kWc": 80,
-            "3-9kWc": 80,
-            "9-36kWc": 180
+            "3-9kWc": 80
         },
-        "tva_reduced": 0.10        # TVA réduite
-    },
+        "tva_reduced": 0.10  # TVA à 10%
+    }
+}
+
     "installation_cost": {         # €/kWc
         "1-3kWc": 2500,
         "3-9kWc": 2200
