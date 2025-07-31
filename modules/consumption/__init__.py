2516c429cb0222fe811aafd9f3c1b0206f6521a2
Blastoche <bastien.laffargue33@gmail.com>
2025-07-31 14:28:03 +0200

    Create __init__.py

---
+++ modules/consumption/__init__.py
@@ -0,0 +1,9 @@
+# =================
+# modules/consumption/__init__.py - NOUVEAU FICHIER
+# =================
+"""
+Module de simulation de consommation électrique
+"""
+from .appliance_models import simulate
+
+__all__ = ['simulate']
